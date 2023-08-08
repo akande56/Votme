8d6da7159aac193b93660e503fd9eb26bec2339b
akande56 <abdulsalamabubakar52@gmail.com>
2023-08-08 12:18:08 +0100

    mmm

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 python -m pip install --upgrade pip
 
-pip install -r requirements.txt
+pip install -r requirements/production.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
